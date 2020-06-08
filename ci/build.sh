302e45f5630026564a652e423cf7c650c43aa5cb
purerosefallen <78877@qq.com>
2020-06-08 17:27:42 +0800

    update ci

--- ci/build.sh
+++ ci/build.sh
@@ -2,11 +2,16 @@
 set -x
 set -o errexit
 DEX_VERSION=$(grep -oP '\[DataEditorX\](.+)\[DataEditorX\]' DataEditorX/readme.txt | sed 's/\[DataEditorX\]//g')
+
+# build
+xbuild /p:Configuration=Release /p:OutDir=$PWD/output/ # /p:TargetFrameworkVersion=v4.6 
+
+# zip tool
+sed -i '/download.mono-project.com/d' /etc/apt/sources.list /etc/apt/sources.list.d/*
 apt update
 apt -y install p7zip-full
 
-xbuild /p:Configuration=Release /p:OutDir=$PWD/output/ # /p:TargetFrameworkVersion=v4.6 
-
+# zip
 mkdir -p dist/releases
 cd output
 7z a -mx9 ../dist/releases/DataEditorX-$DEX_VERSION.zip ./*
