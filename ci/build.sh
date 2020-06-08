5caa5c48f8a4f364f270ba10268abc523d49eae9
purerosefallen <78877@qq.com>
2020-06-08 17:34:58 +0800

    Why the hack this mono image does not contain wget?

--- ci/build.sh
+++ ci/build.sh
@@ -3,17 +3,17 @@ set -x
 set -o errexit
 DEX_VERSION=$(grep -oP '\[DataEditorX\](.+)\[DataEditorX\]' DataEditorX/readme.txt | sed 's/\[DataEditorX\]//g')
 
+# apt packages
+sed -i '/download.mono-project.com/d' /etc/apt/sources.list /etc/apt/sources.list.d/*
+apt update
+apt -y install wget p7zip-full
+
 # data files
 wget -O DataEditorX/data/constant.lua https://koishi.pro/ygopro/script/constant.lua
 wget -O DataEditorX/data/strings.conf https://koishi.pro/ygopro/strings.conf
 
 # build
 xbuild /p:Configuration=Release /p:OutDir=$PWD/output/ /p:TargetFrameworkVersion=v4.6 
-
-# zip tool
-sed -i '/download.mono-project.com/d' /etc/apt/sources.list /etc/apt/sources.list.d/*
-apt update
-apt -y install p7zip-full
 
 # zip
 mkdir -p dist/releases
