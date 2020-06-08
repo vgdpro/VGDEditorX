8b79265ec445b6f0be846f38bcd3f48f7b5faeb7
purerosefallen <78877@qq.com>
2020-06-08 17:50:48 +0800

    other versions

--- DataEditorX/Properties/AssemblyInfo.cs
+++ DataEditorX/Properties/AssemblyInfo.cs
@@ -10,11 +10,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("DataEditorX")]
-[assembly: AssemblyDescription("E-mail:247321452@qq.com")]
+[assembly: AssemblyDescription("E-mail:nanahira@momobako.com")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("DataEditorX")]
-[assembly: AssemblyCopyright("Copyright 2016")]
+[assembly: AssemblyCopyright("Copyright 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -28,4 +28,4 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can use the default the Revision and 
 // Build Numbers by using the '*' as shown below:
-[assembly: AssemblyVersion("2.4.3.6")]
+[assembly: AssemblyVersion("3.0.0.0")]
