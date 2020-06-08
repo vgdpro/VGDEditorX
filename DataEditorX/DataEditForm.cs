f18413f0020739552fe783598de45eeefcda9627
Joy J <316351048@qq.com>
2020-06-08 16:33:16 +0800

    code format

--- DataEditorX/DataEditForm.cs
+++ DataEditorX/DataEditForm.cs
@@ -1352,7 +1352,7 @@ namespace DataEditorX
 			}
 			else
 			{
-				MyTask mt = this.tasker.getLastTask();
+				MyTask mt = this.tasker.GetLastTask();
 				switch (mt)
 				{
 					case MyTask.CheckUpdate:
@@ -2079,7 +2079,7 @@ namespace DataEditorX
 		{
 			Card c = this.GetCard();
 			if(c != null){
-				this.tasker.testPendulumText(c.desc);
+				this.tasker.TestPendulumText(c.desc);
 			}
 		}
 		void Menuitem_export_select_sqlClick(object sender, EventArgs e)
