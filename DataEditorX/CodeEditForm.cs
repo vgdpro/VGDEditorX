908e7b586991a11602ed90f028452786b9b430d1
Joy J <316351048@qq.com>
2020-06-15 11:07:15 +0800

    cleanup

--- DataEditorX/CodeEditForm.cs
+++ DataEditorX/CodeEditForm.cs
@@ -15,7 +15,6 @@ using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.IO;
-using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Windows.Forms;
