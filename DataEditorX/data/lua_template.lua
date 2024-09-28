3cd075e6a052b5da1fee0dda7a81688918f47970
jwyxym <jwyxym@126.com>
2024-09-28 10:32:45 +0800

    Update lua_template.lua

--- DataEditorX/data/lua_template.lua
+++ DataEditorX/data/lua_template.lua
@@ -1,5 +1,6 @@
---𝕃𝕦𝕒 𝔹𝕪 𝕜𝟛 𝕥𝕙𝕖 𝕗𝕒𝕚𝕝𝕦𝕣𝕖
-local this,id,ofs=GetID()
-function this.initial_effect(c)
+--𝔻𝕖𝕩 𝔹𝕪 𝕜𝟛 𝕥𝕙𝕖 𝕗𝕒𝕚𝕝𝕦𝕣𝕖
+--{card_name}
+local cm,m,o=GetID()
+function cm.initial_effect(c)
 	vgf.VgCard(c)
 end
